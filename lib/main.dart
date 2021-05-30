2dc6f54f5c4414c987f8faa564a970460583212a
alemughal <alimug38@gmail.com>
2021-05-30 12:15:45 +0500

    Creating Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: 
-         Text(
-          "Hello World from Ali Mughal! "
+        appBar: AppBar(
+          title: Center(child: Text("Login Page")),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                  width: 220,
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      fillColor: Colors.green,
+                      border: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.green, width: 2.0),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      labelText: 'Email',
+                      focusColor: Colors.green,
+                    ),
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                  width: 220,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.green,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green, width: 2.0),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      labelText: 'Password',
+                      focusColor: Colors.green,
+                    ),
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Login")),
+            ],
+          ),
         ),
       ),
     );
